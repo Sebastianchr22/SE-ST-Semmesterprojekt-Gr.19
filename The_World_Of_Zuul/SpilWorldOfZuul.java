0141cacf102aec0221636001f2ba3b550fc64a54
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2017-09-25 17:02:27 +0200

    Add files via upload

---
+++ The_World_Of_Zuul/SpilWorldOfZuul.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Sebas
+ */
+import worldofzuul.Game;
+public class SpilWorldOfZuul {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Game game = new Game();
+       game.play();
+    }
+    
+}
