c575afb21b7bce3e65a843551e9ed5ff62c40e53
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2017-10-21 14:49:25 +0200

    Somewhat completed private room invite system
    Calculates the chance of a police raid, and hand out a $500 fine for dancing for minors, also rewards the player if the police do not notice a minor during a raid. The code also adds an amount of tips for a successful dance.

--- The_World_Of_Zuul/PrettyWoman/PrivateRoom.java
+++ The_World_Of_Zuul/PrettyWoman/PrivateRoom.java
@@ -1,19 +1,39 @@
 package PrettyWoman;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Obel
  */
 public class PrivateRoom
 {
-    public void test()
-    {
-        System.out.println("Test GitHub");
+    Chance chance = new Chance();
+    PlayerStats stats = new PlayerStats();
+    DanceMech dance = new DanceMech();
+    Regular Sebastian = new Regular(1,"Sebastian",22,"Swaggy","Gold",2,"Silver",3);
+    
+    public void pRoomTips(){
+        double amount = dance.tipsGained(stats.getEnhancements())*3.3+500;
+        stats.addMoneySaved(amount);
+        System.out.println("You gained $" + amount + " from dancing.");
+    }
+    public void privateRoomInvite(Regular regular){
+        regular.getPreference0();
+        if(regular.getAge() < 18){
+            //Razzia might happen:
+            if(chance.ChanceCalc(50, 100)){
+                //Returned true:
+                System.out.println("A police raid just happened and you just got busted dancing for minor.");
+                System.out.println("The police hand you a $500 fine, and you are being held at the police station over night.");
+                System.out.println("You gained some experience from the raid, and from your arrest.");
+                stats.removeMoneySaved(500);
+                stats.addExperience(3);
+            }else{
+                //Returned false:
+                System.out.println("A police raid just happened, thankfully the police did not notice that you were dancing for a minor.");
+                pRoomTips();
+            }
+        }else{
+            //No razzia:
+            pRoomTips();
+        }
     }
 }
