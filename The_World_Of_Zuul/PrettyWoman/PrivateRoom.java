a900df766849a38ae9a929c8ea4ae76442b51fe2
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2017-10-21 16:06:49 +0200

    Still not perfect but can now be run from DanceMech file
    The file has now been updated, so that it will inherite from DanceMech, and can now be run from DanceMech.
    Money gained from dancing in the private room, does not add to the overall MoneySaved variable in the PlayerStats.

--- The_World_Of_Zuul/PrettyWoman/PrivateRoom.java
+++ The_World_Of_Zuul/PrettyWoman/PrivateRoom.java
@@ -1,18 +1,15 @@
 package PrettyWoman;
-public class PrivateRoom
+import java.math.*;
+public class PrivateRoom extends DanceMech
 {
-    Chance chance = new Chance();
-    PlayerStats stats = new PlayerStats();
-    DanceMech dance = new DanceMech();
-    Regular Sebastian = new Regular(1,"Sebastian",22,"Swaggy","Gold",2,"Silver",3);
+    PlayerStats playerStats = new PlayerStats();
     
     public void pRoomTips(){
-        double amount = dance.tipsGained(stats.getEnhancements())*3.3+500;
-        stats.addMoneySaved(amount);
+        double amount = Math.round((tipsGained(playerStats.getEnhancements())*3.3+500)*100.0)/100.0;
         System.out.println("You gained $" + amount + " from dancing.");
+        playerStats.addMoneySaved(amount);
     }
     public void privateRoomInvite(Regular regular){
-        regular.getPreference0();
         if(regular.getAge() < 18){
             //Razzia might happen:
             if(chance.ChanceCalc(50, 100)){
@@ -20,8 +17,8 @@ public class PrivateRoom
                 System.out.println("A police raid just happened and you just got busted dancing for minor.");
                 System.out.println("The police hand you a $500 fine, and you are being held at the police station over night.");
                 System.out.println("You gained some experience from the raid, and from your arrest.");
-                stats.removeMoneySaved(500);
-                stats.addExperience(3);
+                playerStats.removeMoneySaved(500);
+                playerStats.addExperience(3);
             }else{
                 //Returned false:
                 System.out.println("A police raid just happened, thankfully the police did not notice that you were dancing for a minor.");
