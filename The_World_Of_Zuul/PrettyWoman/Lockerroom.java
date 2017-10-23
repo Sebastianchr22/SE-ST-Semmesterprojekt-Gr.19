7a9ae9230d8a7323febdfc777a5fc6cbc72f6029
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2017-10-23 15:43:53 +0200

    Updated to include removing and adding
    now adds to player inventory, and removes item from itemlist.
    Generates a random to pickup a random item, from the pool of items, adds it to inventory, and removes it from the pool.

--- The_World_Of_Zuul/PrettyWoman/Lockerroom.java
+++ The_World_Of_Zuul/PrettyWoman/Lockerroom.java
@@ -10,7 +10,7 @@ package PrettyWoman;
  * @author Sebas
  */
 
-public class Lockerroom extends DanceMech {
+public class Lockerroom {
     public Item weddingring = new Item(0, "Wedding Ring", "Your wedding ring", "Wedding",3);
     public Item goldring = new Item(1, "Gold Ring", "A nice gold ring", "Gold",3);
     public Item goldearring = new Item(2, "Gold Earring", "A nice gold earring", "Gold",3);
@@ -22,20 +22,30 @@ public class Lockerroom extends DanceMech {
     public int genRandom(){
         return (int) Math.round(0+Math.random()*amountOfItems);
     }
-    public void steal(){
-        inv.addToIventory(goldring);
-        inv.addToIventory(goldearring);
-        inv.addToIventory(goldnecklace);
-        inv.addToIventory(goldwristwatch);
-        inv.addToIventory(goldbracelet);
-        inv.showInventory();
+    public void steal(Inventory itemlist, Inventory inv){
+        itemlist.addToIventory(goldring);
+        itemlist.addToIventory(goldearring);
+        itemlist.addToIventory(goldnecklace);
+        itemlist.addToIventory(goldwristwatch);
+        itemlist.addToIventory(goldbracelet);
+        itemlist.showInventory();
         
         int random = genRandom();
-        System.out.println("You found a "+inv.Inventory.get(random).getName());
+        System.out.println("");
+        System.out.println("You found a "+itemlist.Inventory.get(random).getName());
+        inv.addToIventory(itemlist.Inventory.get(random));
+        System.out.println("");
+        itemlist.removeFromInventory(itemlist.Inventory.get(random));
+        System.out.println("");
+        itemlist.showInventory();
     }
     public static void main(String[] args) {
+        Inventory itemlist = new Inventory();
+        Inventory inv = new Inventory();
         Lockerroom locker = new Lockerroom();
-        locker.steal();
+        locker.steal(itemlist,inv);
+        System.out.println("");
+        inv.showInventory();
         
     }
     
