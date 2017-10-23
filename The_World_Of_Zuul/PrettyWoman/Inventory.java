461bb8f3e221fbfad32de108ae7651c54461c7c2
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2017-10-23 13:48:32 +0200

    Capacity checking
    does now return true if you can carry more items, else false.

--- The_World_Of_Zuul/PrettyWoman/Inventory.java
+++ The_World_Of_Zuul/PrettyWoman/Inventory.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 class Inventory extends Item{
     ArrayList<Item> Inventory = new ArrayList<>();
 
-    public int totalSize;
-    public int totalWeight;
+    public int totalSize = 0;
+    public int totalWeight = 0;
     public int itemsCarried = 0;
     public int goldItems = 0;
     public int silverItems = 0;
-    public int Jewelry = 0;
+    public int Outfit = 0;
+    public int wig = 0;
+    public int shoes = 0;
+    public int makeup = 0;
+    public boolean weddingring;
     
     public void addToIventory(Item item){
         Inventory.add(item);
@@ -31,11 +35,40 @@ class Inventory extends Item{
                 case "Silver":
                     silverItems++;
                     break;
+                case "Outfit":
+                    Outfit++;
+                    break;
+                case "Wig":
+                    wig++;
+                    break;
+                case "Shoes":
+                    shoes++;
+                    break;
+                case "Makeup":
+                    makeup++;
+                    break;
+                case "Wedding":
+                    weddingring=true;
+                    break;
             }
             totalSize+=item.getSize();
             itemsCarried++;
             totalWeight=totalSize*5;
         }
+    }
+    public boolean checkCapasity(int itemWeight){
+        countItems();
+        if(totalWeight + itemWeight <= 1000){
+            //Under capasity:
+            return true;
+        }else{
+            //Over capasity:
+            System.out.println("You cannot carry any more.");
+            return false;
+        }
+    }
+    public int getTotalWeight(){
+        return this.totalWeight;
     }
     public void weightToString(){
         System.out.println("Capacity: " + totalWeight + "/1000g");
