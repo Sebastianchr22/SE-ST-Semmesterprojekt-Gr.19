d088835ac52abac75671f7d62f93ee9edfd83ba3
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2017-10-23 09:59:01 +0200

    Update DanceMech.java

--- The_World_Of_Zuul/PrettyWoman/DanceMech.java
+++ The_World_Of_Zuul/PrettyWoman/DanceMech.java
@@ -1,7 +1,5 @@
 package PrettyWoman;
 import java.util.*;
-import java.math.*;
-
 
 public class DanceMech {
     public int move1ExpRequired = 0;
@@ -9,15 +7,14 @@ public class DanceMech {
     PlayerStats playerStats = new PlayerStats();
     Chance chance = new Chance();
     
-    Regular Sebastian = new Regular(1,"Sebastian",22,"Swaggy","Gold",2,"Silver",3);
+    Regular Sebastian = new Regular(1,"Sebastian",12,"Swaggy","Gold",2,"Silver",3);
     
     /**
      * Prints the money and experience gained by the player:
      * @return nothing.
      */
     public void printInterface(){     
-        System.out.println("Money: " + Math.round((playerStats.getMoneySaved())*100.0)/100.0 + "      " + "Exp: " + playerStats.getExperience());//Prints the saved money rounded off to two digits.
-        //playerStats.printMap("FLOOR");
+        System.out.println("$" + Math.round((playerStats.getMoneySaved())*100.0)/100.0 + "      " + "Exp: " + playerStats.getExperience());//Prints the saved money rounded off to two digits.
     }
     
     
@@ -55,26 +52,15 @@ public class DanceMech {
   
 
     
-    /**
-     * Takes experience and improvements to calculate an amout of tips, and add a percentage as a bonus based on your amount
-     * of improvements.
-     * @param Experience is taken from earlier
-     * @param Improvements is taken from earlier
-     * @return returns the amount of money gained as tips.
-     */
-    public double tipsGained(int Improvements) {
-        double percentageBonusTip = 1+Improvements*0.05;//Calculates a percentage of bonus based on improvements
+  
+    public double tipsGained(double bonus) {
+        double percentageBonusTip = 1+playerStats.getEnhancements()*0.05;//Calculates a percentage of bonus based on improvements
         double tips = 5.0+Math.random()*30.0;//Calculates a random number between 5 to 30
-        return Math.round((percentageBonusTip*tips)*100.0)/100.0; //Returns the random* the percentage, rounded off with two digits after the comma.
+        return Math.round((percentageBonusTip*tips+bonus)*100.0)/100.0; //Returns the random* the percentage, rounded off with two digits after the comma.
     }
 
     
-    /**
-     * 
-     * @param danceMoveChoosen is given by the main floor choice
-     * @param Experience is based on the amount of moves the player has performed.
-     * @return 
-     */
+ 
     public void danceMoveYield(String danceMoveChoosen) {
         PrivateRoom pRoom = new PrivateRoom();
         if (danceMoveChoosen.equals("1")) {
@@ -82,7 +68,7 @@ public class DanceMech {
         }
         if (danceMoveChoosen.equals("2")) {
             //Insert from move 1:
-            danceMovePrint(playerStats.getExperience(), move2ExpRequired);// --||--
+            danceMovePrint(playerStats.getExperience(), move2ExpRequired);
         }if(danceMoveChoosen.equals("3")){
             pRoom.privateRoomInvite(Sebastian);
         }if(danceMoveChoosen.equals("map")){
@@ -93,21 +79,13 @@ public class DanceMech {
         }
     }
     
-    
-    /**
-     * this method is called to display the success or failure of a dance move by the player, based on the players experience.
-     * also it displays the amount of tips gained from the move. this is done by calling the function tipsFromMove();. 
-     * And also calling the ChanceCalc method from earlier.
-     * @param experience given from player experience int
-     * @param danceMoveExpRequired given from move requirements
-     * @return does not matter
-     */
+
     public void danceMovePrint(int experience, int danceMoveExpRequired){
         if (experience >= danceMoveExpRequired) {
             //You have enough experience to perform this move:
             System.out.println("Your move was successful.");
             playerStats.addExperience(1);
-            double tipsFromMove = tipsGained(playerStats.getEnhancements());
+            double tipsFromMove = tipsGained(experience-danceMoveExpRequired*3);
             System.out.println("You received $" + tipsFromMove + " in tips for that move.");
             playerStats.addMoneySaved(tipsFromMove);
         } else {
@@ -116,7 +94,7 @@ public class DanceMech {
                 //It was successful:
                 System.out.println("Although you are not experienced with that move, you were successful.");
                 playerStats.addExperience(1);
-                double tipsFromMove = tipsGained(playerStats.getEnhancements());
+                double tipsFromMove = tipsGained(experience-danceMoveExpRequired*3);
                 System.out.println("You received $" + tipsFromMove + " in tips for that move.");
                 playerStats.addMoneySaved(tipsFromMove);
 
