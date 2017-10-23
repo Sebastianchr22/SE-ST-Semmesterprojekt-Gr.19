c3d651fcdd3757f2f032b1e51cb0c7b8cb504159
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2017-10-23 12:42:01 +0200

    Updated as subclass
    subclass to dancemech

--- The_World_Of_Zuul/PrettyWoman/PrivateRoom.java
+++ The_World_Of_Zuul/PrettyWoman/PrivateRoom.java
@@ -1,11 +1,13 @@
 package PrettyWoman;
+
 public class PrivateRoom extends DanceMech
 {    
+    
     public void pRoomTips(int bonus){
-        double amount = Math.round((tipsGained(playerStats.getEnhancements()*6.5+350+bonus))*100.0)/100.0;
+        double amount = Math.round((tipsGained(getEnhancements()*6.5+350+bonus))*100.0)/100.0;
         System.out.println("You gained $" + amount + " from dancing.");
-        System.out.println("You have $"+playerStats.getMoneySaved());
-        playerStats.addMoneySaved(amount);
+        System.out.println("You have $"+getMoneySaved());
+        addMoneySaved(amount);
     }
     public void privateRoomInvite(Regular regular){
         //If the regular is a minor, a raid might happen
@@ -16,8 +18,8 @@ public class PrivateRoom extends DanceMech
                 System.out.println("A police raid just happened and you just got busted dancing for minor.");
                 System.out.println("The police hand you a $500 fine, and you are being held at the police station over night.");
                 System.out.println("You gained some experience from the raid, and from your arrest.");
-                playerStats.removeMoneySaved(500);
-                playerStats.addExperience(3);
+                removeMoneySaved(500);
+                addExperience(3);
             }else{
                 //Returned false:
                 System.out.println("A police raid just happened, thankfully the police did not notice that you were dancing for a minor.");
