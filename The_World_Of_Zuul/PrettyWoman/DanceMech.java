b5eceabac2dc51c352f2384f7f112c495d7f8cff
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2017-10-24 18:34:24 +0200

    Updated version for later use

--- The_World_Of_Zuul/PrettyWoman/DanceMech.java
+++ The_World_Of_Zuul/PrettyWoman/DanceMech.java
@@ -1,9 +1,10 @@
 package PrettyWoman;
 import java.util.*;
 
-public class DanceMech extends PlayerStats {
+public class DanceMech {
     public int move1ExpRequired = 0;
     public int move2ExpRequired = 25;
+    PlayerStats playerStats = new PlayerStats();
     Chance chance = new Chance();
     
     Regular Sebastian = new Regular(1,"Sebastian",12,"Swaggy","Gold",2,"Silver",3);
@@ -13,7 +14,7 @@ public class DanceMech extends PlayerStats {
      * @return nothing.
      */
     public void printInterface(){     
-        System.out.println("$" + Math.round((getMoneySaved())*100.0)/100.0 + "      " + "Exp: " + getExperience());//Prints the saved money rounded off to two digits.
+        System.out.println("$" + Math.round((playerStats.getMoneySaved())*100.0)/100.0 + "      " + "Exp: " + playerStats.getExperience());//Prints the saved money rounded off to two digits.
     }
     
     
@@ -54,7 +55,7 @@ public class DanceMech extends PlayerStats {
     
   
     public double tipsGained(double bonus) {
-        double percentageBonusTip = 1+getEnhancements()*0.05;//Calculates a percentage of bonus based on improvements
+        double percentageBonusTip = 1+playerStats.getEnhancements()*0.05;//Calculates a percentage of bonus based on improvements
         double tips = 5.0+Math.random()*30.0;//Calculates a random number between 5 to 30
         return Math.round((percentageBonusTip*tips+bonus)*100.0)/100.0; //Returns the random* the percentage, rounded off with two digits after the comma.
     }
@@ -64,15 +65,15 @@ public class DanceMech extends PlayerStats {
     public void danceMoveYield(String danceMoveChoosen) {
         PrivateRoom pRoom = new PrivateRoom();
         if (danceMoveChoosen.equals("1")) {
-            danceMovePrint(getExperience(), move1ExpRequired);//Calls the method below to print success or failure.
+            danceMovePrint(playerStats.getExperience(), move1ExpRequired);//Calls the method below to print success or failure.
         }
         if (danceMoveChoosen.equals("2")) {
             //Insert from move 1:
-            danceMovePrint(getExperience(), move2ExpRequired);
+            danceMovePrint(playerStats.getExperience(), move2ExpRequired);
         }if(danceMoveChoosen.equals("3")){
-            pRoom.privateRoomInvite(Sebastian);
+            pRoom.privateRoomInvite(Sebastian,playerStats);
         }if(danceMoveChoosen.equals("map")){
-            printMap("Floor");
+            playerStats.printMap("Floor");
         }
         else {
             //Unknown dancemove
@@ -90,19 +91,19 @@ public class DanceMech extends PlayerStats {
         if (experience >= danceMoveExpRequired) {
             //You have enough experience to perform this move:
             System.out.println("Your move was successful.");
-            addExperience(1);
+            playerStats.addExperience(1);
             double tipsFromMove = tipsGained(exptips(experience,danceMoveExpRequired));
             System.out.println("You received $" + tipsFromMove + " in tips for that move.");
-            addMoneySaved(tipsFromMove);
+            playerStats.addMoneySaved(tipsFromMove);
         } else {
             //You do not have enough experience to perform this move:
             if (chance.ChanceCalc(25, 100)) {
                 //It was successful:
                 System.out.println("Although you are not experienced with that move, you were successful.");
-                addExperience(1);
+                playerStats.addExperience(1);
                 double tipsFromMove = tipsGained(exptips(experience,danceMoveExpRequired));
                 System.out.println("You received $" + tipsFromMove + " in tips for that move.");
-                addMoneySaved(tipsFromMove);
+                playerStats.addMoneySaved(tipsFromMove);
 
             } else {
                 //You failed:
