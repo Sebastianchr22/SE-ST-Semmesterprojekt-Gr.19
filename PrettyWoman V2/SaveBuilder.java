9c822c0c1851360950727c374d6e2dc73ea92d62
Sleaff <32386884+Sleaff@users.noreply.github.com>
2017-11-09 16:36:40 +0100

    setDriver removed from interface and is now private
    setDriver removed from interface and is now private

--- PrettyWoman V2/SaveBuilder.java
+++ PrettyWoman V2/SaveBuilder.java
@@ -3,5 +3,4 @@ import java.io.*;
 public interface SaveBuilder {
     public void saveGame(Driver driver);
     public void loadGame(Driver driver);
-    public void setDriver(String value, int index, Driver driver);
 }
