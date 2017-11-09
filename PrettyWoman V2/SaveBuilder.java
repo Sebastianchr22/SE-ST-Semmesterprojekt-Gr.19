9097e31d9468c2b2661cc0acea4dd7570699b440
Sleaff <32386884+Sleaff@users.noreply.github.com>
2017-11-09 13:29:32 +0100

    Update and rename saveBuilder.java to SaveBuilder.java

--- PrettyWoman V2/SaveBuilder.java
+++ PrettyWoman V2/SaveBuilder.java
@@ -1,6 +1,7 @@
 package PrettyWoman;
 import java.io.*;
-public interface saveBuilder {
+public interface SaveBuilder {
     public void saveGame(Driver driver);
     public void loadGame(Driver driver);
+    public void setDriver(String value, int index, Driver driver);
 }
